e715836fbca15a52dec23a693bf507f1aa9db492
Keri Lamberjack <krlamberjack@gmail.com>
2022-09-30 18:43:44 -0400

    frfrfr ngl

---
+++ gitPractice2.java
@@ -0,0 +1,19 @@
+package GitPractice;
+
+public class gitPractice2 {
+	public static void main(String args[]) {
+		System.out.println("You suck");
+		
+		eiufgaevbaiduviabfvjbaof[vu]argbiuarbgierg
+		
+		fgiubdfakjsdbfkjadskvjiuvkjvasdkjasduf
+		hgy,herjlkhjeawxuh34iojygu3op4ehy
+		HUERGQIUEBGIUQBERGBJ;e98	4ybiejtohgd9niaef
+		
+		
+		erufqeripunkqerghqeroing
+		
+		fieurgbeibgriqergbueo[rugb[wergbqjbrg;kqergbu[qeigpqbegrj]]]
+	}
+	
+}
